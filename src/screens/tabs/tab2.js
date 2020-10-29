a6ccfb81b5dcc4e95efd34981093195bdae87dd1
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-10-30 00:40:22 +0530

    Add files via upload

---
+++ src/screens/tabs/tab2.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+export default class tab2 extends Component
+{
+    render()
+    {
+        return(
+            <View>
+                <Text>Tab 2</Text>
+            </View>
+        );
+    }
+}
---
+++ src/screens/tabs/tab3.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+export default class tab3 extends Component
+{
+    render()
+    {
+        return(
+            <View>
+                <Text>Tab 3</Text>
+            </View>
+        );
+    }
+}
