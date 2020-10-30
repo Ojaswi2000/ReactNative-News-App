d2079a1a02efc2b7696c8dc224a6c6649fb4f91d
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-10-31 02:08:20 +0530

    updated tab2 for showing business news

--- src/screens/tabs/tab2.js
+++ src/screens/tabs/tab2.js
@@ -1,13 +1,53 @@
-import React,{Component} from 'react';
-import {View,Text,StyleSheet} from 'react-native';
-export default class tab2 extends Component
-{
-    render()
-    {
-        return(
-            <View>
-                <Text>Tab 2</Text>
-            </View>
-        );
+import React, { Component } from 'react';
+import { Container, Header, Content, List, ListItem, Thumbnail, Left, Body, Right, Button } from 'native-base';
+import {Alert,Text,ActivityIndicator,View} from 'react-native';
+import {getArticles} from '../../service/news2';
+import DataItem from '../../component/dataItem';
+export default class ListThumbnailExample extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      isLoading:true,
+      data:null
     }
+  }
+  componentDidMount() {
+    getArticles().then(data=>{
+      this.setState({
+        isLoading:false,
+        data: data
+      });
+    }, error =>{
+      Alert.alert('Error','Something went wrong !!');
+    }
+    )
+  }
+
+
+  render() {
+    console.log(this.state.data);
+    let view= this.state.isLoading ? (
+      <View>
+        <ActivityIndicator animating={this.state.isLoading}/>
+        <Text style={{marginTop:10}}>Please Wait...</Text>
+      </View>
+    ):(
+      <List
+            dataArray={this.state.data}
+            renderRow={(item)=>{
+              return <DataItem data={item}/>
+            }}
+          />
+    ) 
+
+    return (
+      <Container>
+        <Content>
+          {view}
+        </Content>
+      </Container>
+    );
+  }
 }
