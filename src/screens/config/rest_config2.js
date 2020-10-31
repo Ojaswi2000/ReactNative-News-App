990a0ab7000672bb475bee15a962567efbb91b5b
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-01 01:28:13 +0530

    Add files via upload

---
+++ src/screens/config/rest_config2.js
@@ -0,0 +1,4 @@
+export const articles_url='http://newsapi.org/v2/top-headlines';
+export const country_code='in';
+export const category='business';
+export const _api_key='aa7d573bfc5d45e99366fb3dc9956f87';
