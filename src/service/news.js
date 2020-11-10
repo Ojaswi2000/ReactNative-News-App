01a42e871fe9170d7a64ff90dbd0842376e9b6e2
Ojaswi Awasthi <65769340+Ojaswi2000@users.noreply.github.com>
2020-11-11 01:12:22 +0530

    updated the default category of the app

--- src/service/news.js
+++ src/service/news.js
@@ -1,5 +1,5 @@
 import {articles_url,category,_api_key,country_code} from '../screens/config/rest_config';
-export async function getArticles()
+export async function getArticles(category="general")
 {
     try{
         let articles=await fetch(`${articles_url}?country=${country_code}&category=${category}`,{
